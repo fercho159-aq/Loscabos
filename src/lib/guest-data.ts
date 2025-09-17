17ad99b496d65aa974104275f151d5f58bee5f86
Fernando Trejo <fernandotrejo159@gmail.com>
2025-09-17 18:17:18 +0000

    biografias

--- src/lib/guest-data.ts
+++ src/lib/guest-data.ts
@@ -6,7 +6,7 @@ export const guestData = [
     bio: "Eugenio Caballero es un diseñador de producción mexicano, ganador del Óscar por El laberinto del fauno. Reconocido por su visión artística, ha colaborado con cineastas de renombre y es una de las figuras más influyentes del cine internacional. En 2025, el Festival Internacional de Cine de Los Cabos le rendirá homenaje con un premio, una masterclass y una retrospectiva completa de su obra.",
     handle: null,
     handleLink: null,
-    imgSrc: "https://picsum.photos/seed/guest1/500/500",
+    imgSrc: "/Images/Biografias/FICC_DECK2025.pdf-image-115.jpg",
     imgHint: "man portrait"
   },
   {
@@ -15,7 +15,7 @@ export const guestData = [
     bio: "Ximena Lamadrid es una actriz reconocida y una de las creativas emergentes más influyentes de México. Productora, directora, actriz, modelo, escritora y artista multidisciplinaria, se mueve con naturalidad entre disciplinas, aportando visión y autenticidad a cada proyecto. Su carrera ha estado marcada por colaboraciones con algunas de las figuras más destacadas del cine, la moda y la cultura. Ya sea en la pantalla, detrás de la cámara o frente a un lente fotográfico, la obra de Lamadrid resuena con una combinación única de arte y relevancia cultural.",
     handle: "@ximenalamadrid",
     handleLink: "https://www.instagram.com/ximenalamadrid",
-    imgSrc: "https://picsum.photos/seed/guest2/500/500",
+    imgSrc: "/Images/Biografias/FICC_DECK2025.pdf-image-118.jpg",
     imgHint: "woman portrait"
   },
   {
@@ -24,7 +24,7 @@ export const guestData = [
     bio: "Gerardo Coello Escalante, joven director mexicano, estrenó Viaje de Negocios en Sundance 2024. Su cine combina mirada íntima y narrativa poderosa, consolidándolo como una de las voces emergentes más prometedoras del cine contemporáneo. Presentará su cortometraje Viaje de Negocios en el festival, y participará en el panel de cineastas jóvenes emergentes.",
     handle: "@gcoello94",
     handleLink: "https://www.instagram.com/gcoello94",
-    imgSrc: "https://picsum.photos/seed/guest3/500/500",
+    imgSrc: "/Images/Biografias/FICC_DECK2025.pdf-image-121.jpg",
     imgHint: "man portrait monochrome"
   },
   {
@@ -33,7 +33,7 @@ export const guestData = [
     bio: "Adolfo Margulis es director y fotógrafo cuya obra une sensibilidad y técnica. Con Spiritum (2024) destacó en Cannes y Morelia. Ha trabajado con marcas globales y figuras como Canelo Álvarez y Shawn Mendes, consolidándose como un creador versátil. Participará en el panel de cineastas jóvenes emergentes y presentará su cortometraje Spiritum.",
     handle: "@adolfo.margulis",
     handleLink: "https://www.instagram.com/adolfo.margulis",
-    imgSrc: "https://picsum.photos/seed/guest4/500/500",
+    imgSrc: "/Images/Biografias/FICC_DECK2025.pdf-image-122.png",
     imgHint: "man portrait glasses"
   },
   {
@@ -42,7 +42,7 @@ export const guestData = [
     bio: "Isabel Vaca, es una directora y productora mexicana. Su ópera prima, Temporada de Campo, se estrenó en Visions du Réel y Hot Docs, y fue reconocida con el Premio del Público en el Festival Internacional de Cine de Morelia. Actualmente dirige su casa productora, Caribou Visual. Sus cortos de ficción Spinning (2023), LOVE (2024) y Matagatos (2025) se presentaron en festivales internacionales como Tribeca, Durban y Morelia.",
     handle: "@isavaca",
     handleLink: "https://www.instagram.com/isavaca",
-    imgSrc: "https://picsum.photos/seed/guest5/500/500",
+    imgSrc: "/Images/Biografias/FICC_DECK2025.pdf-image-130.jpg",
     imgHint: "woman portrait smiling"
   },
   {
@@ -51,7 +51,7 @@ export const guestData = [
     bio: "Arturo Mendicuti es un director y guionista de la Ciudad de México, interesado en contar historias profundas, íntimas, sociales y con buen humor. Su trabajo se ha presentado en festivales como Tribeca, FICM, Visions du Réel y Hot Docs. Su primer largometraje como productor, Temporada de Campo, recibió el Premio del Público en el Festival de Morelia. Matagatos, su más reciente cortometraje, se presentará este año en el FICM.",
     handle: "@AMB",
     handleLink: "https://www.instagram.com/AMB",
-    imgSrc: "https://picsum.photos/seed/guest6/500/500",
+    imgSrc: "/Images/Biografias/FICC_DECK2025.pdf-image-133.jpg",
     imgHint: "man portrait serious"
   },
   {
@@ -60,7 +60,7 @@ export const guestData = [
     bio: "Lau Charles es directora y artista visual egresada del CCC y de “La Esmeralda”. Su cortometraje Casa Chica estrenó en Berlinale Shorts 2025, fue nominado al Oso de Oro y ganó Mejor Cortometraje Mexicano en el GIFF. Su obra, centrada en la infancia, incluye Olote, que fue semifinalista en los BAFTA Student Film Awards. Actualmente desarrolla su ópera prima.",
     handle: "@laucharlesart",
     handleLink: "https://www.instagram.com/laucharlesart",
-    imgSrc: "https://picsum.photos/seed/guest7/500/500",
+    imgSrc: "/Images/Biografias/FICC_DECK2025.pdf-image-136.jpg",
     imgHint: "woman portrait art"
   },
   {
@@ -69,7 +69,7 @@ export const guestData = [
     bio: "María Fernanda Prieto, descubrió su pasión por contar historias desde niña. Sus cortometrajes, entre ellos Recognition y ¿Qué Somos?, se han proyectado en festivales como GLAFF, deadCenter, y LA Shorts Fest. En 2023 dirigió la segunda unidad en la adaptación de Netflix de Pedro Páramo y actualmente desarrolla su primer largometraje con Woo Films. Con cada proyecto, Maria continúa tejiendo historias que capturan la esencia de los momentos honestos de la vida.",
     handle: "marprieto.com",
     handleLink: "http://marprieto.com",
-    imgSrc: "https://picsum.photos/seed/guest8/500/500",
+    imgSrc: "/Images/Biografias/FICC_DECK2025.pdf-image-139.jpg",
     imgHint: "woman portrait professional"
   },
   {
@@ -78,7 +78,7 @@ export const guestData = [
     bio: "Julian Lamadrid es músico y cineasta al frente de una de las bandas de rock electrónico más prometedoras del momento. También escritor, actor y director, actualmente escribe y está listo para dirigir la próxima serie ULTRASOUND. Participara en el panel de cineastas jóvenes emergentes, y su banda The Cave tocaran en vivo, en una fiesta inolvidable.",
     handle: "@thecave.love",
     handleLink: "https://www.instagram.com/thecave.love",
-    imgSrc: "https://picsum.photos/seed/guest9/500/500",
+    imgSrc: "/Images/Biografias/FICC_DECK2025.pdf-image-142.jpg",
     imgHint: "man portrait musician"
   },
   {
@@ -87,7 +87,7 @@ export const guestData = [
     bio: "Martha Sosa Elizondo, es una destacada productora mexicana involucrada en la producción cinematográfica desde 1997. Es ampliamente reconocida por su trabajo en filmes emblemáticos como Amores Perros y Presunto culpable. Sosa cree profundamente en el poder emocional del cine como instrumento para visibilizar problemáticas sociales y generar cambio. Ha impulsado películas que han alcanzado éxito internacional y popularidad masiva.",
     handle: "@marthasosa",
     handleLink: "https://www.instagram.com/marthasosa",
-    imgSrc: "https://picsum.photos/seed/guest10/500/500",
+    imgSrc: "/Images/Biografias/FICC_DECK2025.pdf-image-145.jpg",
     imgHint: "woman portrait elegant"
   }
 ];
