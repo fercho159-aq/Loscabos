e052f9bece818a0322dd3e10cd5553692decf9fc
Fernando Trejo <fernandotrejo159@gmail.com>
2025-09-20 14:48:36 +0000

    este texto se ve muy junto aumenta el interlineado horizontal (_for elem

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -36,7 +36,7 @@ const CardTitle = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "text-2xl font-semibold leading-normal tracking-tight",
+      "text-2xl font-semibold leading-normal",
       className
     )}
     {...props}
