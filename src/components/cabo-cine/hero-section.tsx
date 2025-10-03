c68d6e3ecd3ab1b8b2ef4bcfb4e4f079c1cfb818
Fernando Trejo <fernandotrejo159@gmail.com>
2025-10-03 16:20:48 +0000

    <Button asChild size="lg" variant="link" className="text-accent hover:te

--- src/components/cabo-cine/hero-section.tsx
+++ src/components/cabo-cine/hero-section.tsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
     <section className="relative h-screen w-full flex items-center justify-center text-center text-white overflow-hidden">
       <div className="absolute inset-0 z-0">
         <Image
-          src="https://picsum.photos/seed/plc/1920/1080"
+          src="/Images/Main/Banner inicial.jpg"
           alt="Paisaje de Puerto Los Cabos"
           data-ai-hint="puerto los cabos landscape"
           fill
@@ -39,7 +39,7 @@ export default function HeroSection() {
             <p className="text-base text-background/80 mb-2">
             ¿TE INTERESA ASISTIR?
             </p>
-            <Button asChild size="lg" variant="link" className="text-accent hover:text-accent/90 font-bold text-lg py-3 px-8 group transition-all duration-300 transform hover:scale-105">
+            <Button asChild size="lg" variant="link" className="font-headline text-accent hover:text-accent/90 text-lg py-3 px-8 group transition-all duration-300 transform hover:scale-105">
                 <Link href="/pre-registro">
                     PRE-REGÍSTRATE AQUÍ
                 </Link>
