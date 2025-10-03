b6aa7871cbffc74e72240fb38b6f193bd4bc7f4b
Fernando Trejo <fernandotrejo159@gmail.com>
2025-10-03 16:37:52 +0000

    haslo de 14 px  (_for element p_)

--- src/components/cabo-cine/hero-section.tsx
+++ src/components/cabo-cine/hero-section.tsx
@@ -31,7 +31,7 @@ export default function HeroSection() {
                 />
         </div>
 
-        <p className="mt-4 font-headline text-2xl md:text-3xl text-background tracking-widest">
+        <p className="mt-4 font-headline text-sm text-background tracking-widest">
             #BEYONDTHESCREEN
         </p>
 
