4ece68fc8d2d0e45f0c6abe14f6b99f3d60336c5
Fernando Trejo <fernandotrejo159@gmail.com>
2025-10-06 13:50:13 +0000

    Error: No database connection string was provided to `neon()`. Perhaps a

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 'use server';
-import 'dotenv/config';
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(process.env.DATABASE_URL!);
+const connectionString = 'postgresql://neondb_owner:npg_C2tgjwQu8zUm@ep-green-bar-adztcgj1-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require';
+const sql = neon(connectionString);
 
 export default sql;
