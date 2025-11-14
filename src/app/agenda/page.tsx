fef17085ede5a6125b850856b6e8dc24afc315bf
Fernando Trejo <fernandotrejo159@gmail.com>
2025-11-14 22:25:34 +0000

    Try fixing this error: `Runtime Error: ReferenceError: React is not defi

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import Header from '@/components/cabo-cine/header';
 import Footer from '@/components/cabo-cine/footer';
 import { Badge } from '@/components/ui/badge';
