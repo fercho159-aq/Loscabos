375871552347a1f166749cf2a23ecedb38d73e4c
Fernando Trejo <fernandotrejo159@gmail.com>
2025-11-18 22:14:00 +0000

    actualiza la agenda con estos datos:
    [
      {
        "HORA DE INICIO": "12:00

--- src/lib/agenda-data.ts
+++ src/lib/agenda-data.ts
@@ -14,7 +14,7 @@ export type Event = AgendaEvent;
 export const agendaData: AgendaEvent[] = [
   {
     "HORA DE INICIO": "12:00",
-    "HORA DE FIN": "24:00",
+    "HORA DE FIN": "00:00",
     "EVENTO": "Programación fílmica FICLosCabos",
     "SEDE": "Cinemex San José",
     "CATEGORÍA": "Programa de cine",
@@ -25,7 +25,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "11:00",
     "HORA DE FIN": "12:00",
     "EVENTO": "Panel de cineastas emergentes:\nNuevas caras en el cine",
-    "SEDE": "Hotel El Ganzo",
+    "SEDE": "Hotel El Ganzo Rooftop",
     "CATEGORÍA": "Industria",
     "COLOR": "#051723",
     "Dia": "Jueves 11 Dic"
@@ -34,7 +34,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "12:00",
     "HORA DE FIN": "12:30",
     "EVENTO": "Networking de industria",
-    "SEDE": "Hotel El Ganzo",
+    "SEDE": "Hotel El Ganzo Rooftop",
     "CATEGORÍA": "Industria",
     "COLOR": "#051723",
     "Dia": "Jueves 11 Dic"
@@ -43,7 +43,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "12:30",
     "HORA DE FIN": "13:15",
     "EVENTO": "Brunch & Learn \nFondo Fïlmico Gabriel Figueroa + Anuncio convocatoria 2026",
-    "SEDE": "Hotel El Ganzo",
+    "SEDE": "Hotel El Ganzo Rooftop",
     "CATEGORÍA": "Industria",
     "COLOR": "#051723",
     "Dia": "Jueves 11 Dic"
@@ -87,7 +87,7 @@ export const agendaData: AgendaEvent[] = [
   {
     "HORA DE INICIO": "20:00",
     "HORA DE FIN": "20:45",
-    "EVENTO": "Live Show\nShamans",
+    "EVENTO": "Live Show",
     "SEDE": "Crania",
     "CATEGORÍA": "Music Sessions FICLosCabos",
     "COLOR": "#6688c5",
@@ -97,7 +97,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "20:45",
     "HORA DE FIN": "00:00",
     "EVENTO": "Fiesta Inaugural\nDJ Set",
-    "SEDE": "Crania (Cate)",
+    "SEDE": "Crania Cate",
     "CATEGORÍA": "Music Sessions FICLosCabos",
     "COLOR": "#6688c5",
     "Dia": "Jueves 11 Dic"
@@ -115,7 +115,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "11:15",
     "HORA DE FIN": "12:00",
     "EVENTO": "Conversación de industria por confirmar",
-    "SEDE": "Hotel El Ganzo",
+    "SEDE": "Hotel El Ganzo Rooftop",
     "CATEGORÍA": "Industria",
     "COLOR": "#051723",
     "Dia": "Viernes 12 Dic"
@@ -124,7 +124,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "12:00",
     "HORA DE FIN": "12:30",
     "EVENTO": "Networking de industria",
-    "SEDE": "Hotel El Ganzo",
+    "SEDE": "Hotel El Ganzo Rooftop",
     "CATEGORÍA": "Industria",
     "COLOR": "#051723",
     "Dia": "Viernes 12 Dic"
@@ -133,7 +133,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "12:30",
     "HORA DE FIN": "13:15",
     "EVENTO": "Brunch & Learn \nLa Baja Inspira + Anuncio convocatoria 2026",
-    "SEDE": "Hotel El Ganzo",
+    "SEDE": "Hotel El Ganzo Rooftop",
     "CATEGORÍA": "Industria",
     "COLOR": "#051723",
     "Dia": "Viernes 12 Dic"
@@ -151,7 +151,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "16:00",
     "HORA DE FIN": "18:00",
     "EVENTO": "Conferencia \nLa Conexión Creativa \ncon Luisa Reyes Retana",
-    "SEDE": "Hotel El Ganzo (Galería)",
+    "SEDE": "Hotel El Ganzo Galería",
     "CATEGORÍA": "Programa #BeyondTheScreen",
     "COLOR": "#e67050",
     "Dia": "Viernes 12 Dic"
@@ -168,7 +168,7 @@ export const agendaData: AgendaEvent[] = [
   {
     "HORA DE INICIO": "18:00",
     "HORA DE FIN": "20:00",
-    "EVENTO": "Proyección Especial\nDocumental Depeche Mode M\n+ Q&A con Fernando Frías",
+    "EVENTO": "Proyección Especial\nDocumental Depeche Mode: M\n+ Q&A con Director Fernando Frías",
     "SEDE": "Crania",
     "CATEGORÍA": "Programa #BeyondTheScreen",
     "COLOR": "#e67050",
@@ -185,9 +185,9 @@ export const agendaData: AgendaEvent[] = [
   },
   {
     "HORA DE INICIO": "22:00",
-    "HORA DE FIN": "24:00",
+    "HORA DE FIN": "00:00",
     "EVENTO": "DJ Set",
-    "SEDE": "Crania",
+    "SEDE": "Crania Cate",
     "CATEGORÍA": "Music Sessions FICLosCabos",
     "COLOR": "#6688c5",
     "Dia": "Viernes 12 Dic"
@@ -205,7 +205,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "11:15",
     "HORA DE FIN": "12:00",
     "EVENTO": "Masterclass\ncon Eugenio Caballero",
-    "SEDE": "Hotel El Ganzo (Galería)",
+    "SEDE": "Hotel El Ganzo Galería",
     "CATEGORÍA": "Industria",
     "COLOR": "#051722",
     "Dia": "Sábado 13 Dic"
@@ -214,7 +214,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "12:00",
     "HORA DE FIN": "12:30",
     "EVENTO": "Networking de industria",
-    "SEDE": "Hotel El Ganzo",
+    "SEDE": "Hotel El Ganzo Rooftop",
     "CATEGORÍA": "Industria",
     "COLOR": "#051723",
     "Dia": "Sábado 13 Dic"
@@ -223,7 +223,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "12:30",
     "HORA DE FIN": "13:15",
     "EVENTO": "Brunch & Learn \nIncentivos IMCINE",
-    "SEDE": "Hotel El Ganzo",
+    "SEDE": "Hotel El Ganzo Rooftop",
     "CATEGORÍA": "Industria",
     "COLOR": "#051723",
     "Dia": "Sábado 13 Dic"
@@ -241,7 +241,7 @@ export const agendaData: AgendaEvent[] = [
     "HORA DE INICIO": "17:00",
     "HORA DE FIN": "19:00",
     "EVENTO": "Sesión de poesía performática\nSomos como los ríos\ncon Luisa Reyes Retana y Mercedes Reynosa\n(Seguido por diálogos en torno al territorio y sus cuerpos de agua)",
-    "SEDE": "Hotel El Ganzo",
+    "SEDE": "Hotel El Ganzo Rooftop",
     "CATEGORÍA": "Programa #BeyondTheScreen",
     "COLOR": "#e67050",
     "Dia": "Sábado 13 Dic"
@@ -257,7 +257,7 @@ export const agendaData: AgendaEvent[] = [
   },
   {
     "HORA DE INICIO": "17:45",
-    "HORA DE FIN": "20:00",
+    "HORA DE FIN": "18:30",
     "EVENTO": "Homenaje a Eugenio Caballero",
     "SEDE": "Crania",
     "CATEGORÍA": "Programa #BeyondTheScreen",
@@ -265,8 +265,8 @@ export const agendaData: AgendaEvent[] = [
     "Dia": "Sábado 13 Dic"
   },
   {
-    "HORA DE INICIO": "20:00",
-    "HORA DE FIN": "22:00",
+    "HORA DE INICIO": "18:30",
+    "HORA DE FIN": "20:30",
     "EVENTO": "Proyección Especial en Homenaje a Eugenio Caballero\nUn monstruo viene a verme",
     "SEDE": "Crania",
     "CATEGORÍA": "Programa #BeyondTheScreen",
@@ -274,10 +274,10 @@ export const agendaData: AgendaEvent[] = [
     "Dia": "Sábado 13 Dic"
   },
   {
-    "HORA DE INICIO": "22:00",
+    "HORA DE INICIO": "20:30",
     "HORA DE FIN": "00:00",
     "EVENTO": "Space Xmas\nEvento aliado",
-    "SEDE": "Crania",
+    "SEDE": "Crania Cate",
     "CATEGORÍA": "Music Sessions FICLosCabos",
     "COLOR": "#6688c5",
     "Dia": "Sábado 13 Dic"
@@ -292,8 +292,8 @@ export const agendaData: AgendaEvent[] = [
     "Dia": "Domingo 14 Dic"
   },
   {
-    "HORA DE INICIO": "18:00",
-    "HORA DE FIN": "19:00",
+    "HORA DE INICIO": "17:30",
+    "HORA DE FIN": "18:30",
     "EVENTO": "Proyección de cortometrajes animados\nen alianza con Cut Out Fest\n\nAbierto al público con acceso gratuito",
     "SEDE": "Jetty",
     "CATEGORÍA": "Programa de cine",
@@ -301,8 +301,8 @@ export const agendaData: AgendaEvent[] = [
     "Dia": "Domingo 14 Dic"
   },
   {
-    "HORA DE INICIO": "19:00",
-    "HORA DE FIN": "21:30",
+    "HORA DE INICIO": "18:30",
+    "HORA DE FIN": "21:00",
     "EVENTO": "Amores Perros\nProyección gratuita\n\nAbierto al público con acceso gratuito",
     "SEDE": "Jetty",
     "CATEGORÍA": "Programa de cine",
@@ -323,7 +323,7 @@ export const groupedAgenda = agendaData.reduce((acc, event) => {
 
 const allVenues = [...new Set(agendaData.map(e => e['SEDE']))];
 export const venues = allVenues.sort((a,b) => {
-    const order = ["Hotel El Ganzo", "Hotel El Ganzo (Galería)", "Crania", "Crania (Cate)", "Cinemex San José", "Jetty"];
+    const order = ["Hotel El Ganzo Rooftop", "Hotel El Ganzo Galería", "Crania", "Crania Cate", "Cinemex San José", "Jetty"];
     return order.indexOf(a) - order.indexOf(b);
 });
 
@@ -346,3 +346,5 @@ function generateDetailedTimeSlots() {
 
 export const detailedTimeSlots = generateDetailedTimeSlots();
 
+
+    
