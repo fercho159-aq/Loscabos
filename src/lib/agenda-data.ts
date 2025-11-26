ac36de1a9bbe7c555e8c742207537bb4a04fd7aa
Fernando Trejo <fernandotrejo159@gmail.com>
2025-11-26 14:46:04 +0000

    actualizacion de datos

--- src/lib/agenda-data.ts
+++ src/lib/agenda-data.ts
@@ -12,271 +12,277 @@ export type AgendaEvent = {
 export type Event = AgendaEvent;
 
 export const agendaData: AgendaEvent[] = [
- 
-  {
-    "HORA DE INICIO": "11:00",
-    "HORA DE FIN": "12:00",
-    "EVENTO": "Panel de cineastas emergentes: Nuevas caras en el cine",
-    "SEDE": "Hotel El Ganzo Rooftop",
-    "CATEGORÍA": "Industria",
-    "COLOR": "#051723",
-    "Dia": "jueves 11 dic"
-  },
-  {
-    "HORA DE INICIO": "12:00",
-    "HORA DE FIN": "12:30",
-    "EVENTO": "Networking de industria",
-    "SEDE": "Hotel El Ganzo Rooftop",
-    "CATEGORÍA": "Industria",
-    "COLOR": "#051723",
-    "Dia": "jueves 11 dic"
-  },
-  {
-    "HORA DE INICIO": "12:30",
-    "HORA DE FIN": "13:15",
-    "EVENTO": "Brunch & Learn Fondo Fïlmico Gabriel Figueroa + Anuncio convocatoria 2026",
-    "SEDE": "Hotel El Ganzo Rooftop",
-    "CATEGORÍA": "Industria",
-    "COLOR": "#051723",
-    "Dia": "jueves 11 dic"
-  },
-  {
-    "HORA DE INICIO": "15:00",
-    "HORA DE FIN": "17:00",
-    "EVENTO": "Happy Hour",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "jueves 11 dic"
-  },
-  {
-    "HORA DE INICIO": "17:30",
-    "HORA DE FIN": "18:00",
-    "EVENTO": "Inauguración oficial Host: José María Yazpik",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "jueves 11 dic"
-  },
-  {
-    "HORA DE INICIO": "18:00",
-    "HORA DE FIN": "18:15",
-    "EVENTO": "Live Cinema Performance Personas Haciendo Cosas con Tania Reza",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "jueves 11 dic"
-  },
-  {
-    "HORA DE INICIO": "18:15",
-    "HORA DE FIN": "20:00",
-    "EVENTO": "Proyección especial Largometraje Sorpresa",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "jueves 11 dic"
-  },
-  {
-    "HORA DE INICIO": "20:00",
-    "HORA DE FIN": "20:45",
-    "EVENTO": "Live Show",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Music Sessions FICLosCabos",
-    "COLOR": "#6688c5",
-    "Dia": "jueves 11 dic"
-  },
-  {
-    "HORA DE INICIO": "20:45",
-    "HORA DE FIN": "00:00",
-    "EVENTO": "Fiesta Inaugural DJ Set",
-    "SEDE": "Crania Cate",
-    "CATEGORÍA": "Music Sessions FICLosCabos",
-    "COLOR": "#6688c5",
-    "Dia": "jueves 11 dic"
-  },
- 
-  {
-    "HORA DE INICIO": "11:15",
-    "HORA DE FIN": "12:00",
-    "EVENTO": "Conversación de industria (por confirmar)",
-    "SEDE": "Hotel El Ganzo Rooftop",
-    "CATEGORÍA": "Industria",
-    "COLOR": "#051723",
-    "Dia": "viernes 12 de dic"
-  },
-  {
-    "HORA DE INICIO": "12:00",
-    "HORA DE FIN": "12:30",
-    "EVENTO": "Networking de industria",
-    "SEDE": "Hotel El Ganzo Rooftop",
-    "CATEGORÍA": "Industria",
-    "COLOR": "#051723",
-    "Dia": "viernes 12 de dic"
-  },
-  {
-    "HORA DE INICIO": "12:30",
-    "HORA DE FIN": "13:15",
-    "EVENTO": "Brunch & Learn La Baja Inspira + Anuncio convocatoria 2026",
-    "SEDE": "Hotel El Ganzo Rooftop",
-    "CATEGORÍA": "Industria",
-    "COLOR": "#051723",
-    "Dia": "viernes 12 de dic"
-  },
-  {
-    "HORA DE INICIO": "15:00",
-    "HORA DE FIN": "17:00",
-    "EVENTO": "Happy Hour",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "viernes 12 de dic"
-  },
-  {
-    "HORA DE INICIO": "16:00",
-    "HORA DE FIN": "18:00",
-    "EVENTO": "Conferencia La Conexión Creativa con Luisa Reyes Retana",
-    "SEDE": "Hotel El Ganzo Galería",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "viernes 12 de dic"
-  },
-  {
-    "HORA DE INICIO": "17:30",
-    "HORA DE FIN": "18:00",
-    "EVENTO": "Proyección especial El Pulmo con presentación de Mark Kronemeyer y Begoña Félix",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "viernes 12 de dic"
-  },
-  {
-    "HORA DE INICIO": "18:00",
-    "HORA DE FIN": "20:00",
-    "EVENTO": "Proyección Especial Documental Depeche Mode: M + Q&A con Director Fernando Frías",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "viernes 12 de dic"
-  },
-  {
-    "HORA DE INICIO": "20:00",
-    "HORA DE FIN": "22:00",
-    "EVENTO": "Proyección Especial Ed Maverick, La Nube en el jardín en vivo desde la sala Nezahualcóyotl + Conversatorio",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "viernes 12 de dic"
-  },
-  {
-    "HORA DE INICIO": "22:00",
-    "HORA DE FIN": "00:00",
-    "EVENTO": "DJ Set",
-    "SEDE": "Crania Cate",
-    "CATEGORÍA": "Music Sessions FICLosCabos",
-    "COLOR": "#6688c5",
-    "Dia": "viernes 12 de dic"
-  },
+    {
+      "HORA DE INICIO": "11:00",
+      "HORA DE FIN": "12:00",
+      "EVENTO": "Panel de cineastas emergentes:\nNuevas caras en el cine",
+      "SEDE": "Hotel El Ganzo Rooftop",
+      "CATEGORÍA": "Industria",
+      "COLOR": "#051723",
+      "Dia": "jueves 11 dic"
+    },
+    {
+      "HORA DE INICIO": "12:00",
+      "HORA DE FIN": "12:30",
+      "EVENTO": "Networking de industria",
+      "SEDE": "Hotel El Ganzo Rooftop",
+      "CATEGORÍA": "Industria",
+      "COLOR": "#051723",
+      "Dia": "jueves 11 dic"
+    },
+    {
+      "HORA DE INICIO": "12:30",
+      "HORA DE FIN": "13:15",
+      "EVENTO": "Brunch & Learn \nFondo Fïlmico Gabriel Figueroa + Anuncio convocatoria 2026",
+      "SEDE": "Hotel El Ganzo Rooftop",
+      "CATEGORÍA": "Industria",
+      "COLOR": "#051723",
+      "Dia": "jueves 11 dic"
+    },
+    {
+      "HORA DE INICIO": "16:00",
+      "HORA DE FIN": "18:00",
+      "EVENTO": "Happy Hour",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "jueves 11 dic"
+    },
+    {
+      "HORA DE INICIO": "18:15",
+      "HORA DE FIN": "18:40",
+      "EVENTO": "Inauguración oficial\nMaestro de ceremonia: José María Yazpik",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "jueves 11 dic"
+    },
+    {
+      "HORA DE INICIO": "18:40",
+      "HORA DE FIN": "18:55",
+      "EVENTO": "Live Cinema Performance \nPersonas Haciendo Cosas con Tania Reza",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "jueves 11 dic"
+    },
+    {
+      "HORA DE INICIO": "19:00",
+      "HORA DE FIN": "20:55",
+      "EVENTO": "Proyección especial\nLos domingos | Dir. Alauda Ruiz de Azua | España | 115 min.",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "jueves 11 dic"
+    },
+    {
+      "HORA DE INICIO": "21:00",
+      "HORA DE FIN": "21:45",
+      "EVENTO": "Live Show\nShamans",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Music Sessions FICLosCabos",
+      "COLOR": "#6688c5",
+      "Dia": "jueves 11 dic"
+    },
+    {
+      "HORA DE INICIO": "21:45",
+      "HORA DE FIN": "0:00",
+      "EVENTO": "Fiesta Inaugural\nDJ Set\nMandrake",
+      "SEDE": "Crania Cate",
+      "CATEGORÍA": "Music Sessions FICLosCabos",
+      "COLOR": "#6688c5",
+      "Dia": "jueves 11 dic"
+    },
+    {
+      "HORA DE INICIO": "11:15",
+      "HORA DE FIN": "12:00",
+      "EVENTO": "Conversación de industria (por confirmar)",
+      "SEDE": "Hotel El Ganzo Rooftop",
+      "CATEGORÍA": "Industria",
+      "COLOR": "#051723",
+      "Dia": "viernes 12 de dic"
+    },
+    {
+      "HORA DE INICIO": "12:00",
+      "HORA DE FIN": "12:30",
+      "EVENTO": "Networking de industria",
+      "SEDE": "Hotel El Ganzo Rooftop",
+      "CATEGORÍA": "Industria",
+      "COLOR": "#051723",
+      "Dia": "viernes 12 de dic"
+    },
+    {
+      "HORA DE INICIO": "12:30",
+      "HORA DE FIN": "13:15",
+      "EVENTO": "Brunch & Learn \nLa Baja Inspira + Anuncio convocatoria 2026",
+      "SEDE": "Hotel El Ganzo Rooftop",
+      "CATEGORÍA": "Industria",
+      "COLOR": "#051723",
+      "Dia": "viernes 12 de dic"
+    },
+    {
+      "HORA DE INICIO": "16:00",
+      "HORA DE FIN": "18:00",
+      "EVENTO": "Happy Hour",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "viernes 12 de dic"
+    },
+    {
+      "HORA DE INICIO": "16:00",
+      "HORA DE FIN": "18:00",
+      "EVENTO": "Conferencia \nLa Conexión Creativa \ncon Luisa Reyes Retana",
+      "SEDE": "Hotel El Ganzo Galería",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "viernes 12 de dic"
+    },
+    {
+      "HORA DE INICIO": "18:10",
+      "HORA DE FIN": "18:40",
+      "EVENTO": "Proyección especial \nEl Pulmo | Dir. Mark Kronemeyer y Begoña Félix | México | 30 min.",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "viernes 12 de dic"
+    },
+    {
+      "HORA DE INICIO": "19:00",
+      "HORA DE FIN": "21:00",
+      "EVENTO": "Proyección Especial\nDepeche Mode: M | Dir. Fernando Frías | EEUU, México | 95 min.\nQ&A",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "viernes 12 de dic"
+    },
+    {
+      "HORA DE INICIO": "21:00",
+      "HORA DE FIN": "23:05",
+      "EVENTO": "Proyección Especial\nLa Nube En El Jardín En Vivo desde La Sala Nezahualcóyotl | Dir. Olmo Guerra | México | 99 min.\nQ&A",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "viernes 12 de dic"
+    },
+    {
+      "HORA DE INICIO": "23:05",
+      "HORA DE FIN": "1:00",
+      "EVENTO": "DJ Set",
+      "SEDE": "Crania Cate",
+      "CATEGORÍA": "Music Sessions FICLosCabos",
+      "COLOR": "#6688c5",
+      "Dia": "viernes 12 de dic"
+    },
+    {
+      "HORA DE INICIO": "11:15",
+      "HORA DE FIN": "12:00",
+      "EVENTO": "Masterclass\ncon Eugenio Caballero",
+      "SEDE": "Hotel El Ganzo Galería",
+      "CATEGORÍA": "Industria",
+      "COLOR": "#051722",
+      "Dia": "sábado 13 de dic"
+    },
+    {
+      "HORA DE INICIO": "12:00",
+      "HORA DE FIN": "12:30",
+      "EVENTO": "Networking de industria",
+      "SEDE": "Hotel El Ganzo Rooftop",
+      "CATEGORÍA": "Industria",
+      "COLOR": "#051723",
+      "Dia": "sábado 13 de dic"
+    },
+    {
+      "HORA DE INICIO": "12:30",
+      "HORA DE FIN": "13:15",
+      "EVENTO": "Brunch & Learn \nIncentivos IMCINE",
+      "SEDE": "Hotel El Ganzo Rooftop",
+      "CATEGORÍA": "Industria",
+      "COLOR": "#051723",
+      "Dia": "sábado 13 de dic"
+    },
+    {
+      "HORA DE INICIO": "16:00",
+      "HORA DE FIN": "18:00",
+      "EVENTO": "Happy Hour",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "sábado 13 de dic"
+    },
+    {
+      "HORA DE INICIO": "17:00",
+      "HORA DE FIN": "19:00",
+      "EVENTO": "Sesión de poesía performática\nSomos como los ríos\ncon Luisa Reyes Retana y Mercedes Reynosa\n(Seguido por diálogos en torno al territorio y sus cuerpos de agua)",
+      "SEDE": "Hotel El Ganzo Rooftop",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "sábado 13 de dic"
+    },
+    {
+      "HORA DE INICIO": "17:30",
+      "HORA DE FIN": "17:45",
+      "EVENTO": "Proyección especial \nPesca con futuro",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "sábado 13 de dic"
+    },
+    {
+      "HORA DE INICIO": "18:00",
+      "HORA DE FIN": "18:30",
+      "EVENTO": "Homenaje a Eugenio Caballero",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "sábado 13 de dic"
+    },
+    {
+      "HORA DE INICIO": "18:30",
+      "HORA DE FIN": "20:18",
+      "EVENTO": "Proyección Especial en Homenaje a Eugenio Caballero\nUn monstruo viene a verme | Dir. Juan Antonio Bayona | España, EEUU | 108 min.",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "sábado 13 de dic"
+    },
+    {
+      "HORA DE INICIO": "20:20",
+      "HORA DE FIN": "20:40",
+      "EVENTO": "Premiación\nCompetencia FICLosCabos de largometrajes mexicanos + Palabras de clausura",
+      "SEDE": "Crania",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "sábado 13 de dic"
+    },
+    {
+      "HORA DE INICIO": "20:40",
+      "HORA DE FIN": "0:00",
+      "EVENTO": "Space Xmas\nEvento aliado",
+      "SEDE": "Crania Cate",
+      "CATEGORÍA": "Music Sessions FICLosCabos",
+      "COLOR": "#6688c5",
+      "Dia": "sábado 13 de dic"
+    },
+    {
+      "HORA DE INICIO": "16:30",
+      "HORA DE FIN": "18:20",
+      "EVENTO": "Proyección especial\nLalo y Luquín van a la baja | Dir. Jorge Curioca | México | 110 min\n\n*Abierto al público | Acceso gratuito*",
+      "SEDE": "Jetty",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "domingo 14 de dic"
+    },
+    {
+      "HORA DE INICIO": "18:30",
+      "HORA DE FIN": "21:04",
+      "EVENTO": "Proyección especial\nAmores Perros | Alejandro González Iñárritu | México | 154 min.\n\nAbierto al público con acceso gratuito",
+      "SEDE": "Jetty",
+      "CATEGORÍA": "Programa #BeyondTheScreen",
+      "COLOR": "#e67050",
+      "Dia": "domingo 14 de dic"
+    }
   
-  {
-    "HORA DE INICIO": "11:15",
-    "HORA DE FIN": "12:00",
-    "EVENTO": "Masterclass con Eugenio Caballero",
-    "SEDE": "Hotel El Ganzo Galería",
-    "CATEGORÍA": "Industria",
-    "COLOR": "#051722",
-    "Dia": "sábado 13 de dic"
-  },
-  {
-    "HORA DE INICIO": "12:00",
-    "HORA DE FIN": "12:30",
-    "EVENTO": "Networking de industria",
-    "SEDE": "Hotel El Ganzo Rooftop",
-    "CATEGORÍA": "Industria",
-    "COLOR": "#051723",
-    "Dia": "sábado 13 de dic"
-  },
-  {
-    "HORA DE INICIO": "12:30",
-    "HORA DE FIN": "13:15",
-    "EVENTO": "Brunch & Learn Incentivos IMCINE",
-    "SEDE": "Hotel El Ganzo Rooftop",
-    "CATEGORÍA": "Industria",
-    "COLOR": "#051723",
-    "Dia": "sábado 13 de dic"
-  },
-  {
-    "HORA DE INICIO": "15:00",
-    "HORA DE FIN": "17:00",
-    "EVENTO": "Happy Hour",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "sábado 13 de dic"
-  },
-  {
-    "HORA DE INICIO": "17:00",
-    "HORA DE FIN": "19:00",
-    "EVENTO": "Sesión de poesía performática Somos como los ríos con Luisa Reyes Retana y Mercedes Reynosa (Seguido por diálogos en torno al territorio y sus cuerpos de agua)",
-    "SEDE": "Hotel El Ganzo Rooftop",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "sábado 13 de dic"
-  },
-  {
-    "HORA DE INICIO": "17:30",
-    "HORA DE FIN": "17:45",
-    "EVENTO": "Proyección especial Pesca con futuro",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "sábado 13 de dic"
-  },
-  {
-    "HORA DE INICIO": "17:45",
-    "HORA DE FIN": "18:30",
-    "EVENTO": "Homenaje a Eugenio Caballero",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "sábado 13 de dic"
-  },
-  {
-    "HORA DE INICIO": "18:30",
-    "HORA DE FIN": "20:30",
-    "EVENTO": "Proyección Especial en Homenaje a Eugenio Caballero Un monstruo viene a verme",
-    "SEDE": "Crania",
-    "CATEGORÍA": "Programa #BeyondTheScreen",
-    "COLOR": "#e67050",
-    "Dia": "sábado 13 de dic"
-  },
-  {
-    "HORA DE INICIO": "20:30",
-    "HORA DE FIN": "00:00",
-    "EVENTO": "Space Xmas Evento aliado",
-    "SEDE": "Crania Cate",
-    "CATEGORÍA": "Music Sessions FICLosCabos",
-    "COLOR": "#6688c5",
-    "Dia": "sábado 13 de dic"
-  },
- 
-  {
-    "HORA DE INICIO": "17:30",
-    "HORA DE FIN": "18:30",
-    "EVENTO": "Proyección de cortometrajes animados en alianza con Cut Out Fest. Abierto al público con acceso gratuito",
-    "SEDE": "Jetty",
-    "CATEGORÍA": "Programa de cine",
-    "COLOR": "#3a3a08",
-    "Dia": "domingo 14 de dic"
-  },
-  {
-    "HORA DE INICIO": "18:30",
-    "HORA DE FIN": "21:00",
-    "EVENTO": "Amores Perros. Proyección gratuita. Abierto al público con acceso gratuito",
-    "SEDE": "Jetty",
-    "CATEGORÍA": "Programa de cine",
-    "COLOR": "#3a3a09",
-    "Dia": "domingo 14 de dic"
-  }
 ];
 
 export const groupedAgenda = agendaData.reduce((acc, event) => {
